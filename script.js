ba4ed138653e406a916599d5a4d7897b74edc8d5
Maruf1075 <marufcu98@gmail.com>
2023-10-20 14:26:08 +0600

    Added initial files

---
+++ script.js
@@ -0,0 +1,48 @@
+// scroll Selection
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick= () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar link
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let header= document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+// typed js 
+const typed = new Typed('.multiple-text',{
+    strings: ['Frontend Developer','Programmer','Problem Solver'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
